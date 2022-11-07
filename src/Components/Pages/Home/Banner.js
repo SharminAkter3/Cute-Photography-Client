4c03201239d7bbd877df0bf01ff2265ac740d34a
SharminAkter3 <mssharmin4145@gmail.com>
2022-11-07 12:13:15 -0800

    added bannar

--- src/Components/Pages/Home/Banner.js
+++ src/Components/Pages/Home/Banner.js
@@ -1,16 +1,16 @@
-// import React from 'react';
+import React from 'react';
 
-// const Banner = () => {
-//     return (
-//         <div className="carousel w-full">
-//             <div id="item1" className="carousel-item w-50">
-//                 <img src="https://images.hindustantimes.com/img/2021/08/19/1600x900/patrick-dozkVhDyvhQ-unsplash_1628163163817_1629346849962.jpg" alt='' className="w-3/4 h-4/5 p-10 rounded-2xl" />
-//                 <div className="absolute transform -translate-y-1/2 top-1/2">
-//                     <p className='text-5xl mx-10 p-10 font-medium'>Welcome To My <small className='text-5xl font-bold'>Cute<br />Photography</small> Website</p>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+const Banner = () => {
+    return (
+        <div className="carousel w-full">
+            <div id="item1" className="carousel-item w-50">
+                <img src="https://images.hindustantimes.com/img/2021/08/19/1600x900/patrick-dozkVhDyvhQ-unsplash_1628163163817_1629346849962.jpg" alt='' className="w-3/4 h-4/5 p-10 rounded-2xl" />
+                <div className="absolute transform -translate-y-1/2 top-1/2">
+                    <p className='text-5xl mx-10 p-10 font-medium'>Welcome To My <small className='text-5xl font-bold'>Cute<br />Photography</small> Website</p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-// export default Banner;
+export default Banner;
