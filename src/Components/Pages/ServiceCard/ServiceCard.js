68ac34b6ca8082752af95747c1578d08d1ee08bd
SharminAkter3 <mssharmin4145@gmail.com>
2022-11-08 02:49:08 -0800

    service card add

--- src/Components/Pages/ServiceCard/ServiceCard.js
+++ src/Components/Pages/ServiceCard/ServiceCard.js
@@ -2,31 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ServiceCard = ({ service }) => {
-    // const { name, img, price, description } = service;
-    // return (
-    //     <div>
-    //         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-    //             <figure><img className='w-full h-96' src={img} alt="Shoes" /></figure>
-    //             <div className="card-body">
-    //                 <h2 className="card-title text-2xl font-bold">{name}</h2>
-    //                 <h2 className='text-xl text-blue-800 font-semibold'>Price: ${price}</h2>
+    const { name, img, price, description } = service;
+    return (
+        <div>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure><img className='w-full h-96' src={img} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title text-2xl font-bold">{name}</h2>
+                    <h2 className='text-xl text-blue-800 font-semibold'>Price: ${price}</h2>
 
-    //                 <div className=''>
-    //                     {
-    //                         description.length > 150 ?
-    //                             <p>{description.slice(0, 150) + '...'}<Link className='text-primary'>read more</Link></p>
+                    <div className=''>
+                        {
+                            description.length > 150 ?
+                                <p>{description.slice(0, 150) + '...'}<Link className='text-primary'>read more</Link></p>
 
-    //                             :
-    //                             <p>{description}</p>
-    //                     }
-    //                 </div>
-    //                 <div className="card-actions justify-end">
-    //                     <button className="btn btn-outline btn-primary"> <Link>Details</Link></button>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
+                                :
+                                <p>{description}</p>
+                        }
+                    </div>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-outline btn-primary"> <Link>Details</Link></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ServiceCard;
