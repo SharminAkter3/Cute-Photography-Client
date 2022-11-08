99b0c7f0aaadd816795c9c5ecc6dbf2452e59cb3
SharminAkter3 <mssharmin4145@gmail.com>
2022-11-08 10:16:18 -0800

    service details card

--- src/Components/Pages/Details/Details.js
+++ src/Components/Pages/Details/Details.js
@@ -1,24 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import ServiceDetails from './ServiceDetails';
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Details = () => {
-    const [services, setServices] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:5000/services')
-            .then(res => res.json())
-            .then(data => setServices(data))
-    })
+    const { name, img, price, description } = useLoaderData();
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-7 p-10'>
             <div>
-                <h1>Service Details</h1>
-                {
-                    services.map(details => <ServiceDetails
-                        key={details._id}
-                        details={details}
-                    ></ServiceDetails>
-                    )
-                }
+                <h1 className='text-3xl font-bold m-4'>Service Details </h1>
+                <div className="card card-compact w-full bg-base-100 shadow-xl">
+                    <figure><img className='w-full h-96' src={img} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-2xl font-bold">{name}</h2>
+                        <h2 className='text-xl text-blue-800 font-semibold'>Price: ${price}</h2>
+
+                        <div className=''>
+                            <p>{description}</p>
+                        </div>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-outline btn-primary">Details</button>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div>
                 <h1>Review Section</h1>
--- src/Routes/Router/Router.js
+++ src/Routes/Router/Router.js
@@ -27,7 +27,8 @@ export const router = createBrowserRouter([
             },
             {
                 path: '/details/:id',
-                element: <Details></Details>
+                element: <Details></Details>,
+                loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
             },
             {
                 path: '/blog',
